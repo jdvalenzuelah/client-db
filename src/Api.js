f158c040bfa2860bf0543bf645c2cec98dd4612e
David Valenzuela <33351729+val171001@users.noreply.github.com>
2019-06-04 07:48:11 -0600

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: 'https://server-db-uvg.herokuapp.com/'
+    baseURL: 'https://server-db-v2.herokuapp.com/'
   })
 }
